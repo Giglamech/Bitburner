b96d5f3202f960165142e276ced54b7edd8dc07b
Giglamech <82039307+Giglamech@users.noreply.github.com>
2022-01-16 23:36:30 +0100

    Create runGeneralHack.js

---
+++ runGeneralHack.js
@@ -0,0 +1,10 @@
+/** @param {NS} ns **/
+export async function main(ns) {
+	var threads = ns.args[0];
+	ns.run("generalHack.js", threads, "hong-fang-tea");
+	ns.run("generalHack.js", threads, "harakiri-sushi");
+	ns.run("generalHack.js", threads, "joesguns");
+	ns.run("generalHack.js", threads, "sigma-cosmetics");
+	ns.run("generalHack.js", threads, "foodnstuff");
+	ns.run("generalHack.js", threads, "n00dles");
+}
