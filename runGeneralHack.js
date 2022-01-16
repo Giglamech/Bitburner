8dc951a66add3573471c19e4d4dbff4de497636b
Giglamech <82039307+Giglamech@users.noreply.github.com>
2022-01-17 00:08:55 +0100

    Update runGeneralHack.js

--- runGeneralHack.js
+++ runGeneralHack.js
@@ -1,7 +1,6 @@
 /** @param {NS} ns **/
 export async function main(ns) {
 	var threads = ns.args[0];
-	await ns.wget("https://cdn.jsdelivr.net/gh/Giglamech/Bitburner@latest/generalHack.js", "generalHack.js");
 	ns.run("generalHack.js", threads, "hong-fang-tea");
 	ns.run("generalHack.js", threads, "harakiri-sushi");
 	ns.run("generalHack.js", threads, "joesguns");
